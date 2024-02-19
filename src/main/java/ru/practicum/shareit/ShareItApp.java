5beec867e01a4bbaf4adf6ce38ee114f70a36142
Sergey <45943782+sergeyk13@users.noreply.github.com>
2024-02-19 11:16:44 +0300

    подготовка к add-item-requests

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class ShareItApp {
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.practicum.shareit.item.dto.ItemResponseDto;
 import ru.practicum.shareit.item.dto.ItemResponseToRequestDto;
 
 import javax.validation.constraints.Min;
