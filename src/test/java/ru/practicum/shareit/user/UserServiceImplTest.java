be59b2cad88e21c80baf3b518b2b5761481b6933
Sergey <45943782+sergeyk13@users.noreply.github.com>
2024-02-19 11:37:59 +0300

    add-item-requests 1.3.1

--- src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -5,7 +5,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import ru.practicum.shareit.error.model.NotFoundException;
-import ru.practicum.shareit.error.model.ValidationException;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.model.UserDtoResponse;
 import ru.practicum.shareit.user.model.UserUpdateRequest;
@@ -13,6 +12,7 @@ import ru.practicum.shareit.user.model.UserUpdateRequest;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -29,7 +29,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void updateUserName() {
+    void updateUser() {
         long userId = 1L;
         UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
         userUpdateRequest.setName("New Name");
@@ -40,6 +40,7 @@ class UserServiceImplTest {
         existingUser.setEmail("old@example.com");
 
         when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(any(User.class))).thenReturn(existingUser);
 
         UserDtoResponse updatedUser = userService.updateUser(userId, userUpdateRequest);
 
@@ -48,65 +49,7 @@ class UserServiceImplTest {
         assertEquals(existingUser.getEmail(), updatedUser.getEmail());
 
         verify(userRepository, times(2)).findById(userId);
-    }
-
-    @Test
-    void updateUserEmail() {
-        long userId = 1L;
-        UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
-        userUpdateRequest.setEmail("new@example.com");
-
-        User existingUser = new User();
-        existingUser.setId(userId);
-        existingUser.setName("Old Name");
-        existingUser.setEmail("old@example.com");
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
-
-        UserDtoResponse updatedUser = userService.updateUser(userId, userUpdateRequest);
-
-        assertEquals(userId, updatedUser.getId());
-        assertEquals(existingUser.getName(), updatedUser.getName());
-        assertEquals(userUpdateRequest.getEmail(), updatedUser.getEmail());
-
-        verify(userRepository, times(2)).findById(userId);
-    }
-
-    @Test
-    void updateUserEmailAndName() {
-        long userId = 1L;
-        UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
-        userUpdateRequest.setName("New Name");
-        userUpdateRequest.setEmail("new@example.com");
-
-        User existingUser = new User();
-        existingUser.setId(userId);
-        existingUser.setName("Old Name");
-        existingUser.setEmail("old@example.com");
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
-
-        UserDtoResponse updatedUser = userService.updateUser(userId, userUpdateRequest);
-
-        assertEquals(userId, updatedUser.getId());
-        assertEquals(userUpdateRequest.getName(), updatedUser.getName());
-        assertEquals(userUpdateRequest.getEmail(), updatedUser.getEmail());
-
-        verify(userRepository, times(2)).findById(userId);
-    }
-
-    @Test
-    void shouldNpdateUserWithoutEmailAndName() {
-        long userId = 1L;
-        UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
-
-        User existingUser = new User();
-        existingUser.setId(userId);
-        existingUser.setName("Old Name");
-        existingUser.setEmail("old@example.com");
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
-        assertThrows(ValidationException.class, () -> userService.updateUser(userId,userUpdateRequest));
+        verify(userRepository, times(1)).save(any(User.class));
     }
 
     @Test
