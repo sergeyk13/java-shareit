1e6a412993e7d8fe33e30114c7c481497cd77da5
Sergey <45943782+sergeyk13@users.noreply.github.com>
2024-03-22 14:41:38 +0300

    Подготовка - реализовал Item, User - переделал Booking - сделал правильную структуру проекта

--- src/main/java/ru/practicum/shareit/error/model/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.error.model;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
-
